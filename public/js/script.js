4cd28f703f5958a2161c118ad01cdd0333d82091
sutiawann <wawansutiawan257910@gmail.com>
2023-10-09 12:35:59 +0700

    slidernew

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,21 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+
+// Tab
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname) {
+//     for (tablink of tablinks) {
+//         tablink.classList.remove("active-link");
+//     }
+//     for (tabcontent of tabcontents) {
+//         tabcontent.classList.remove("active-tab");
+//     }
+//     event.currentTarget.classList.add("active-link");
+//     document.getElementById(tabname).classList.add("active-tab");
+// }
+
